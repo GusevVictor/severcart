1eea1c12076918732b771d69f301aafa162327bb
User <sfcl@mail.ru>
2015-10-26 21:51:55 +0500

    Глобальное изменене стилей кнопок

--- static/js/detect.js
+++ static/js/detect.js
@@ -1,10 +1,9 @@
 var agt = navigator.userAgent.toLowerCase(),
-	IE8 = (agt.indexOf('msie 8.0') != -1) //&& document.all && !OP,
-	IE7 = (agt.indexOf('msie 7.0') != -1) //&& document.all && !OP,
-	IE6 = (agt.indexOf('msie 6.0') != -1) //&& document.all && !OP,
+	IE8 = (agt.indexOf('msie 8.0') != -1),
+	IE7 = (agt.indexOf('msie 7.0') != -1), 
+	IE6 = (agt.indexOf('msie 6.0') != -1)
 
 	if (IE8) {
-        //return 1;
         ;
     } else {
     
