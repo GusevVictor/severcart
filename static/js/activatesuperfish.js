5555572aac9bb41f574d00fa1afe901960c67e7b
User <sfcl@mail.ru>
2015-11-21 14:33:17 +0500

    #5. Убран надоедливый треугольник в выпадающем меню.

--- static/js/activatesuperfish.js
+++ static/js/activatesuperfish.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function() {
-		jQuery('#menu').superfish();
+		jQuery('#menu').superfish(
+			{
+				delay:       500,                            // one second delay on mouseout
+				animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation
+				speed:       'fast',                          // faster animation speed
+				autoArrows:  false ,                           // disable generation of arrow mark-up
+				cssArrows:   false,
+			});
 });
