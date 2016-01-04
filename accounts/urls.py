a8553c5f33b62750039ffc37562436ae81b318a7
Maxim <sfcl@mail.ru>
2016-01-04 21:41:59 +0600

    Исправил RemovedInDjango110Warning

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls import url
-# urlpatterns = patterns(
-#     url(r'register$', 'accounts.views.register', name='register'),
-#     url(r'login$', 'accounts.views.login', name='login'),
-#     url(r'logout$', 'accounts.views.logout', name='logout'),
-# )
+from index.views import manage_users
+from accounts.views import register
+from accounts.views import edit_user
+from accounts.views import login
+from accounts.views import logout
+from accounts.views import edit
+from accounts.views import delete
 
 urlpatterns = [
-    url(r'^$', 'index.views.manage_users', name='manage_users'),
-    url(r'^add_user/', 'accounts.views.register', name='register'),
-    url(r'^edit_user/', 'accounts.views.edit_user', name='edit_user'),
-    url(r'^login/', 'accounts.views.login', name='login'),
-    url(r'^logout/', 'accounts.views.logout', name='logout'),
-    url(r'^edit/', 'accounts.views.edit', name='edit'),
-    url(r'^delete/', 'accounts.views.delete', name='del'),
+    url(r'^$', manage_users, name='manage_users'),
+    url(r'^add_user/', register, name='register'),
+    url(r'^edit_user/', edit_user, name='edit_user'),
+    url(r'^login/', login, name='login'),
+    url(r'^logout/', logout, name='logout'),
+    url(r'^edit/', edit, name='edit'),
+    url(r'^delete/', delete, name='del'),
 ]
--- index/api/urls.py
+++ index/api/urls.py
@@ -1,11 +1,17 @@
 
 from django.conf.urls import include, url
+from index.api.views import city_list
+from index.api.views import inx
+from index.api.views import upd_dashboard_tbl
+from index.api.views import del_node
+from index.api.views import turf_cartridge
+from accounts.api.views import del_users
 
 urlpatterns = [
-    url('^city_list/', 'index.api.views.city_list'),
-	url('^inx/', 'index.api.views.inx'),
-	url('^upd_dashboard_tbl/', 'index.api.views.upd_dashboard_tbl'),
-	url('^del_node/', 'index.api.views.del_node'),
-	url('^turf_cartridge/', 'index.api.views.turf_cartridge'),
-	url('^del_users/', 'accounts.api.views.del_users')
+    url('^city_list/', city_list),
+	url('^inx/', inx),
+	url('^upd_dashboard_tbl/', upd_dashboard_tbl),
+	url('^del_node/', del_node),
+	url('^turf_cartridge/', turf_cartridge),
+	url('^del_users/', del_users)
 ]
