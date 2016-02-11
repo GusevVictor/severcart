0da05298be0de6b18d0a0580fe4e6ab9efd6a266
Maxim <sfcl@mail.ru>
2016-02-11 10:45:45 +0500

    Bugfixes

--- newskald_ru/settings.py
+++ newskald_ru/settings.py
@@ -108,6 +108,8 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
