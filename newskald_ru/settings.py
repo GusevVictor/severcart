1182e027f0e4aea9f265d3dbf53cb9f577c30bc7
Maxim <sfcl@mail.ru>
2016-02-11 12:19:34 +0500

    For PaaS

--- newskald_ru/settings.py
+++ newskald_ru/settings.py
@@ -110,9 +110,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+    STATIC_ROOT,
+)
 
 STATIC_URL = '/static/'
 
