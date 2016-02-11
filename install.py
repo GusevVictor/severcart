c39cd2300ac41ecf6fccd252277276bdf92b5fe7
Maxim <sfcl@mail.ru>
2016-02-11 17:46:28 +0500

    bugfixes

--- install.py
+++ install.py
@@ -5,12 +5,14 @@ import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newskald_ru.settings")
 
 from accounts.models import AnconUser
-user = AnconUser(username='Admin', is_admin = True)
+user = AnconUser(username='admin', is_admin = True)
 p1 = input('Enter password: ')
 p2 = input('Enter password again: ')
+
 
 if p1 == p2:
 	user.set_password('root')
 	user.save()
+	print('User admin create.')
 else:
 	print('User not created. Passwords not equal.')
