da6aa2ebcd617946ac4b2023886c4812eeca743c
Maxim <sfcl@mail.ru>
2016-02-11 14:14:29 +0500

    For PaaS

--- newskald_ru/settings.py
+++ newskald_ru/settings.py
@@ -110,9 +110,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#STATICFILES_DIRS = (
-#    STATIC_ROOT,
-#)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 STATIC_URL = '/static/'
 
