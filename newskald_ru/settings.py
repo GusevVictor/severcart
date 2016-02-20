cef9f1df17777b003bb478a8cf0178a693f0c8a4
Maxim <sfcl@mail.ru>
2016-02-20 13:36:48 +0500

    Diactivate superfish.js menu

--- newskald_ru/settings.py
+++ newskald_ru/settings.py
@@ -83,7 +83,7 @@ if DEBUG:
                     'django.template.context_processors.request',
                     'django.contrib.auth.context_processors.auth',
                     'django.contrib.messages.context_processors.messages',
-                    'django.core.context_processors.static',
+ #                   'django.core.context_processors.static',
                     'django.template.context_processors.static',
                 ],
                 'loaders': [
@@ -105,7 +105,7 @@ else:
                     'django.template.context_processors.request',
                     'django.contrib.auth.context_processors.auth',
                     'django.contrib.messages.context_processors.messages',
-                    'django.core.context_processors.static',
+#                    'django.core.context_processors.static',
                     'django.template.context_processors.static',
                 ],
                 'loaders': [
