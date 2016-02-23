8047de5843e0a7647104468a675f203269b16b54
unknown <@R89.loc>
2016-02-23 15:26:10 +0500

    Init GUI installer

---
+++ tkinstaller/install.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import tkinter as tk
+
+class SevercartInstaller(object):
+    def __init__(self, master)
+         self.master = master
+         self.master.title("Severcart installer GUI")
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    my_gui = SevercartInstaller(root)
+    root.mainloop()
+    
