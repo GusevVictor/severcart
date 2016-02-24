d736b3343ba90dc2f197ad88ee07bf327a9432e2
unknown <@R89.LOC>
2016-02-24 23:17:58 +0500

    Реализация основных возможнеостей установщика

--- tkinstaller/install.py
+++ tkinstaller/install.py
@@ -6,6 +6,9 @@ import subprocess
 import psycopg2
 import tkinter as tk
 
+user_name     = ''
+user_password = ''
+
 def set_window_center(obj):
     obj.update_idletasks()
     ws = obj.winfo_screenwidth()
@@ -63,6 +66,10 @@ class Page2(object):
         self.page2.destroy()
 
     def new_window(self):
+        global user_name
+        user_name = self.login.get()
+        global user_password
+        user_password = self.password.get()
         self.app = Page3(parent=self.page2)
 
 class Page3(object):
@@ -71,6 +78,7 @@ class Page3(object):
     def __init__(self, parent):
         self.no_errors = False
         self.base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        self.django_base_path = os.path.join(self.base_path, 'ancon', 'newskald_ru')
         self.postgresql_db_path = ''
         self.postgresql_bin_path = ''
         self.proc = ''
@@ -97,16 +105,10 @@ class Page3(object):
         self.page3.destroy()
 
     def oexit(self):
-        self.proc.terminate()
-        self.proc.kill()
+        self.proc = subprocess.Popen([self.postgresql_bin_path, 'stop', '-D', self.postgresql_db_path])
         sys.exit(1)
 
     def action(self):
-        # некоторые действия
-        self.no_errors = True
-        if self.no_errors:
-            self.page3_next = tk.Button(self.page3, text='Далее', command=self.new_window)
-            self.page3_next.pack(fill=tk.X)
         # основные манипуляции по установке ПП Severcart
         # запуск базы данных
         self.postgresql_db_path = os.path.join(self.base_path,
@@ -120,17 +122,21 @@ class Page3(object):
                                             'pg_ctl.exe')
 
         # start postgresql        
-        #C:\work\OpenServer\modules\database\PostgreSQL-9.4\bin\pg_ctl
-         #start -D "c:/work/openserver/userdata/PostgreSQL-9.4"
-        self.text.insert(tk.END, 'Start PostgreSQL' + '\n')
         db_ready = False
         try:
-            self.proc = subprocess.Popen([self.postgresql_bin_path, 'start', '-D', self.postgresql_db_path])
+            conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password=''")
         except:
-            self.text.insert(tk.END, 'PostgreSQL not start.' + '\n')
+            pass
         else:
-            self.text.insert(tk.END, 'Start PostgreSQL success.' + '\n')
             db_ready = True
+        if  not db_ready:
+            try:
+                self.proc = subprocess.Popen([self.postgresql_bin_path, 'start', '-D', self.postgresql_db_path])
+            except:
+                self.text.insert(tk.END, 'PostgreSQL not start.' + '\n')
+            else:
+                self.text.insert(tk.END, 'Start PostgreSQL success.' + '\n')
+                db_ready = True
 
         if db_ready:
             # create db severcart
@@ -139,12 +145,63 @@ class Page3(object):
             except:
                 self.text.insert(tk.END, 'I am unable to connect to the database.' + '\n')
             else:
-                 self.text.insert(tk.END, 'Connect PostgreSQL success.' + '\n')
-            
+                self.text.insert(tk.END, 'Connect PostgreSQL success.' + '\n')
 
-          
-        #for i in range(100):
-        #    self.text.insert(tk.END, str(i)+ '\n')
+            sql_create_base = """
+                CREATE DATABASE severcart
+                WITH OWNER = postgres
+                ENCODING = 'UTF8'
+                TABLESPACE = pg_default
+                LC_COLLATE = 'Russian_Russia.1251'
+                LC_CTYPE = 'Russian_Russia.1251'
+                CONNECTION LIMIT = -1;
+            """
+            try:
+                cur = conn.cursor()
+                conn.set_isolation_level(0)
+                cur.execute(sql_create_base)
+                conn.set_isolation_level(1)
+            except psycopg2.ProgrammingError as e:
+                self.text.insert(tk.END, str(e) + '\n')
+            else:
+                self.text.insert(tk.END, 'Database severcart created.' + '\n')
+
+            # migrate python schema
+            sys.path.append(os.path.join(self.base_path, 'ancon'))
+            sys.path.append(os.path.join(self.base_path, 'ancon', 'newskald_ru'))
+            sys.path.append(os.path.join(self.base_path, 'venv', 'Scripts'))
+            # Activate your virtual env
+            activate_env=os.path.expanduser(os.path.join(self.base_path, 'venv', 'Scripts', 'activate_this.py'))
+            with open(activate_env) as f:
+                code = compile(f.read(), activate_env, 'exec')
+                exec(code, dict(__file__=activate_env))
+
+            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newskald_ru.settings-prod')
+            from django.core.management import execute_from_command_line
+            migrate_ok = False
+            try:
+                execute_from_command_line(['manage.py', 'migrate'])
+            except:
+                self.text.insert(tk.END, 'В процессе миграции произошла ошибка.' + '\n')
+            else:
+                self.text.insert(tk.END, 'Схема успешно мигрирована.' + '\n')
+                migrate_ok = True
+
+            if migrate_ok:
+                # создание нового административного пользователя
+                from accounts.models import AnconUser
+                try:
+                    user = AnconUser(username=user_name, is_admin = True)
+                    user.set_password(user_password)
+                    user.save()
+                except:
+                    self.text.insert(tk.END, 'В процессе создания пользователя возникли ошибки.' + '\n')
+                else:
+                    self.text.insert(tk.END, 'Пользователь успешно создан.' + '\n')
+                    self.page3_next = tk.Button(self.page3, text='Далее', command=self.new_window)
+                    self.page3_next.pack(fill=tk.X)
+
+
     def new_window(self):
         self.app = Page4(parent=self.page3)
 
