cf0bb67404f91b790f369a81fb868fbd31f8ec34
Maxim Khozyainov <sfcl@mail.ru>
2016-04-06 17:35:52 +0500

    Добавленна форма ввода данных подключения к почтовому серверу.

--- service/forms/input_server_settings.py
+++ service/forms/input_server_settings.py
@@ -6,14 +6,11 @@ from django.utils.translation import ugettext_lazy as _
 class SMTPsettings(forms.Form):
     """Ввод информации для подключению к SMTP серверу
     """
-    server_address = forms.CharField(label=_('Sever address'))
-    server_port    = forms.IntegerField(label=_('Sever port'))
+    smtp_server    = forms.CharField(label=_('Sever address'))
+    smtp_port      = forms.IntegerField(label=_('Sever port'))
     email_sender   = forms.EmailField(label=_('Email sender'))
     smtp_login     = forms.CharField(label=_('SMTP login'))
-    smtp_password  = forms.CharField(label=_('SMTP password'), widget=forms.PasswordInput)
+    smtp_password  = forms.CharField(label=_('SMTP password'))
     use_ssl        = forms.BooleanField(label=_('Use SSL'), required=False)
 
     required_css_class = 'required'
-    
-    #def user_id_clean(self):
-        
--- service/views.py
+++ service/views.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from common.helpers import is_admin
+from django.utils.translation import ugettext_lazy as _
+from .forms.input_server_settings import SMTPsettings
+from service.models import Settings
+from .helpers import SevercartConfigs
 
 @login_required
 @is_admin
@@ -8,3 +16,63 @@ def submenu(request):
     """
     """
     return render(request, 'service/submenu.html', context={})
+
+
+
+@login_required
+@is_admin
+def settings_mail(request):
+    """Форма настройки почтового ящика, с которого будут приходить уведомления
+    """
+    context = dict()
+    if request.method == 'POST':
+        form = SMTPsettings(request.POST)
+        if form.is_valid():
+            data_in_post   = form.cleaned_data
+            smtp_server    = data_in_post['smtp_server']
+            smtp_port      = data_in_post['smtp_port']
+            email_sender   = data_in_post['email_sender']
+            smtp_login     = data_in_post['smtp_login']
+            smtp_password  = data_in_post['smtp_password']
+            use_ssl        = data_in_post['use_ssl']
+
+            # сохраняем введённые данные в настроечную таблицу
+            conf = SevercartConfigs()
+            conf.smtp_server    = smtp_server
+            conf.smtp_port      = smtp_port
+            conf.email_sender   = email_sender
+            conf.smtp_login     = smtp_login
+            conf.smtp_password  = smtp_password
+            conf.use_ssl        = use_ssl
+            conf.commit()
+            messages.success(request, _('Settings successfully saved!') )
+            return HttpResponseRedirect(request.path)
+        else:
+            context['form'] = form
+    elif request.method == 'GET':
+        try:
+            m1 = Settings.objects.get(pk=1)
+        except Settings.DoesNotExist:
+            form = SMTPsettings(initial={
+                'smtp_server'   : '',
+                'smtp_port'     : '',
+                'email_sender'  : '',
+                'smtp_login'    : '',
+                'smtp_password' : '',
+                'use_ssl'       : '',
+            })
+        else:
+            # если таблица уже содержит данные, то инициализируем внутренние переменные
+            form = SMTPsettings(initial={
+                'smtp_server'   : m1.smtp_server,
+                'smtp_port'     : m1.smtp_port,
+                'email_sender'  : m1.email_sender,
+                'smtp_login'    : m1.smtp_login,
+                'smtp_password' : m1.smtp_password,
+                'use_ssl'       : m1.use_ssl,
+            })
+            #
+        context['form'] = form
+    else:
+        context['error'] = _('Not implemented method.')
+    return render(request, 'service/settings_mail.html', context)
