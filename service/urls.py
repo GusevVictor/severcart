ce06b63ac40164bc5848a9003c5a18947558355e
Maxim Khozyainov <sfcl@mail.ru>
2016-04-06 12:17:57 +0500

    Новыя страница ввода информации по подключению к почтовому серверу

--- service/urls.py
+++ service/urls.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 
 from django.conf.urls import url, include
-from .views import submenu
+from .views import submenu, settings_mail
 
 urlpatterns = [
     url(r'^$', submenu, name='submenu'),
+    url(r'settings_mail/', settings_mail, name='settings_mail'),
 ]
