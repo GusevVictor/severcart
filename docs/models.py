652defbeebcb04128f58ddb626ba56b0d10a6457
Maxim Khozyainov <sfcl@mail.ru>
2016-04-28 12:24:35 +0500

    Добавленна поддержка новых полей scdoc.date_created и scdoc.spent

--- docs/models.py
+++ docs/models.py
@@ -13,15 +13,17 @@ class SCDoc(models.Model):
         (3, _('Deed of conveyance')),
         (4, _('Act of write-off')),
     )
-    number      = models.CharField(db_index=True, max_length=256)
-    date        = models.DateField(db_index=True, null=True)
-    firm        = models.ForeignKey(FirmTonerRefill, null=True)
-    title       = models.CharField(max_length=256)
-    short_cont  = models.TextField(null=True)
-    money       = models.IntegerField(db_index=True, null=True)
-    departament = models.ForeignKey(OrganizationUnits)
-    doc_type    = models.IntegerField(choices=DOC_TYPE)
-    user        = models.CharField(max_length=256, null=True)
+    number       = models.CharField(db_index=True, max_length=256)
+    date         = models.DateField(db_index=True, null=True) # дата договора
+    date_created = models.DateField(db_index=True, null=True) # дата добавления договора в СУБД
+    firm         = models.ForeignKey(FirmTonerRefill, null=True)
+    title        = models.CharField(max_length=256)
+    short_cont   = models.TextField(null=True)
+    money        = models.IntegerField(db_index=True, null=True)
+    spent        = models.IntegerField(db_index=True, null=True)
+    departament  = models.ForeignKey(OrganizationUnits)
+    doc_type     = models.IntegerField(choices=DOC_TYPE)
+    user         = models.CharField(max_length=256, null=True)
 
     def __str__(self):
         return ('%s %s') % (self.number, self.title,)
