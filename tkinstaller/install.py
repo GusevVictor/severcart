20ab860437d0d77036bbdfdf5a218312c02f6c36
Maxim Khozyainov <sfcl@mail.ru>
2016-07-04 01:34:32 -0400

    Switch to prod

--- tkinstaller/install.py
+++ tkinstaller/install.py
@@ -144,6 +144,17 @@ if __name__ == '__main__':
         print('Дальнейшее продолжение установки невозможно!')
         prompt_exit()
     else:
+        print('-------------------------------------------------')
+        print('--Генерация ключа подписи сессионной переменной--')
+        print('-------------------------------------------------')
+        from django.utils.crypto import get_random_string
+        import json
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        secret_key = get_random_string(50, chars)
+        SECRETS = dict()
+        SECRETS['secret_key'] = secret_key
+        with open(os.path.join(PROJ_DIR, 'conf', 'secrets.json'), 'w') as j:
+            json.dump(SECRETS, j)
         # производим запуск миграции схемы Severcart и Django        
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
@@ -194,17 +205,6 @@ if __name__ == '__main__':
                 # возвращаемся к началу цикла
                 flag = True
 
-        print('-------------------------------------------------')
-        print('--Генерация ключа подписи сессионной переменной--')
-        print('-------------------------------------------------')
-        from django.utils.crypto import get_random_string
-        import json
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        secret_key = get_random_string(50, chars)
-        SECRETS = dict()
-        SECRETS['secret_key'] = secret_key
-        with open(os.path.join(PROJ_DIR, 'conf', 'secrets.json'), 'w') as j:
-            json.dump(SECRETS, j)
         print('-------------------------------------------------')
         print('----------Установка успешно завершена------------')
         print('-------------------------------------------------')
