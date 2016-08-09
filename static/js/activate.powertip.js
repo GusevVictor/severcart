378341e79468e2ac9adc21f192283ab2c7f625a4
Maxim Khozyainov <sfcl@mail.ru>
2016-08-09 14:16:44 +0500

    Bugfix

--- static/js/activate.powertip.js
+++ static/js/activate.powertip.js
@@ -4,12 +4,16 @@ $( function() {
         smartPlacement: true
     });
 
-    $('.info_ico').powerTip({
-        placement: 'ne', // north-east tooltip position
-        smartPlacement: true
+
+    $('.info_ico').each(function() {
+        $(this).powerTip({
+            placement: 'ne', // north-east tooltip position
+            smartPlacement: true
+        });
+
+        $(this).click( function(){
+            $(this).powerTip('show');
+        });
     });
 
-    $('.info_ico').click( function(){
-        $('.info_ico').powerTip('show');
-    } );
 });
