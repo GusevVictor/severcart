dcfa59d357abd9afd7b6a516dffbb39eafe68f3b
Maxim Khozyainov <sfcl@mail.ru>
2016-08-16 16:47:10 +0500

    Поддержка SMTP без аутентификации. Close issue #87

--- service/forms/input_server_settings.py
+++ service/forms/input_server_settings.py
@@ -9,9 +9,33 @@ class SMTPsettings(forms.Form):
     smtp_server    = forms.CharField(label=_('Sever address'), widget=forms.TextInput(attrs={'class': 'inline left'}))
     smtp_port      = forms.IntegerField(label=_('Sever port'))
     email_sender   = forms.EmailField(label=_('Email sender'))
-    smtp_login     = forms.CharField(label=_('SMTP login'))
-    smtp_password  = forms.CharField(label=_('SMTP password'), max_length=32, widget=forms.PasswordInput)
+    smtp_login     = forms.CharField(label=_('SMTP login'), required=False)
+    smtp_password  = forms.CharField(label=_('SMTP password'), max_length=32, widget=forms.PasswordInput, required=False)
     use_ssl        = forms.BooleanField(label=_('Use SSL'), required=False)
     use_tls        = forms.BooleanField(label=_('Use TLS'), required=False)
 
     required_css_class = 'required'
+
+    def clean_smtp_port(self):
+        """Проверяем введённый номер порта на область допустимых значений.
+        """
+        smtp_port = self.cleaned_data.get('smtp_port', 0)
+        if (smtp_port >=1) and (smtp_port <= 65535):
+            return smtp_port    
+        else:
+            raise forms.ValidationError(_('This port number is invalid'))
+
+
+    def clean_smtp_login(self):
+        """Очищаем логин пользователя от посторонних симолов.
+        """
+        smtp_login = self.cleaned_data.get('smtp_login', '')
+        smtp_login = smtp_login.strip()
+        smtp_login =  None if smtp_login == '' else smtp_login
+        return smtp_login
+
+    def clean_smtp_password(self):
+        """Очищаем пароль пользователя от посторонних симолов.
+        """
+        smtp_password = self.cleaned_data.get('smtp_password', None)
+        return smtp_password
