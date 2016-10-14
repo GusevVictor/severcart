473b81be6b226c7faa19b0851fda80f3867276ac
Maxim Khozyainov <sfcl@mail.ru>
2016-10-14 17:48:56 +0500

    Добавил фильтр для поля time

--- index/forms/add_items.py
+++ index/forms/add_items.py
@@ -8,6 +8,58 @@ from docs.models  import SCDoc
 from storages.models import Storages
 
 
+TIME = (
+    ('', ''),
+    (0, '00:00'),
+    (1, '00:30'),
+    (2, '01:00'),
+    (3, '01:30'),
+    (4, '02:00'),
+    (5, '02:30'),
+    (6, '03:00'),
+    (7, '03:30'),
+    (8, '04:00'),
+    (9, '04:30'),
+    (10, '05:00'),
+    (11, '05:30'),
+    (12, '06:00'),
+    (13, '06:30'),
+    (14, '07:00'),
+    (15, '07:30'),
+    (16, '08:00'),
+    (17, '08:30'),
+    (18, '09:00'),
+    (19, '09:30'),
+    (20, '10:00'),
+    (21, '10:30'),
+    (22, '11:00'),
+    (23, '11:30'),
+    (24, '12:00'),
+    (25, '12:30'),
+    (26, '13:00'),
+    (27, '13:30'),
+    (28, '14:00'),
+    (29, '14:30'),
+    (30, '15:00'),
+    (31, '15:30'),
+    (32, '16:00'),
+    (33, '16:30'),
+    (34, '17:00'),
+    (35, '17:30'),
+    (36, '18:00'),
+    (37, '18:30'),
+    (38, '19:00'),
+    (39, '19:30'),
+    (40, '20:00'),
+    (41, '20:30'),
+    (42, '21:00'),
+    (43, '21:30'),
+    (44, '22:00'),
+    (45, '22:30'),
+    (46, '23:00'),
+    (47, '23:30'),
+)
+
 class AddItems(forms.Form):
     manualNumber = forms.CharField(max_length = 256, required=False)
     cartName = forms.ModelChoiceField(queryset=CartridgeItemName.objects.all(),
@@ -27,56 +79,6 @@ class AddItems(forms.Form):
 
     date = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'datepicker', 'readonly':'readonly'}))
     
-    TIME = (
-        ('', ''),
-        (0, '00:00'),
-        (1, '00:30'),
-        (2, '01:00'),
-        (3, '01:30'),
-        (4, '02:00'),
-        (5, '02:30'),
-        (6, '03:00'),
-        (7, '03:30'),
-        (8, '04:00'),
-        (9, '04:30'),
-        (10, '05:00'),
-        (11, '05:30'),
-        (12, '06:00'),
-        (13, '06:30'),
-        (14, '07:00'),
-        (15, '07:30'),
-        (16, '08:00'),
-        (17, '08:30'),
-        (18, '09:00'),
-        (19, '09:30'),
-        (20, '10:00'),
-        (21, '10:30'),
-        (22, '11:00'),
-        (23, '11:30'),
-        (24, '12:00'),
-        (25, '12:30'),
-        (26, '13:00'),
-        (27, '13:30'),
-        (28, '14:00'),
-        (29, '14:30'),
-        (30, '15:00'),
-        (31, '15:30'),
-        (32, '16:00'),
-        (33, '16:30'),
-        (34, '17:00'),
-        (35, '17:30'),
-        (36, '18:00'),
-        (37, '18:30'),
-        (38, '19:00'),
-        (39, '19:30'),
-        (40, '20:00'),
-        (41, '21:30'),
-        (42, '22:00'),
-        (43, '22:30'),
-        (44, '23:00'),
-        (45, '23:30'),
-    )
-
     time = forms.ChoiceField(choices=TIME, required=False)
 
     required_css_class = 'required'
@@ -121,3 +123,26 @@ class AddItems(forms.Form):
             raise ValidationError(_('Required field.'))
         
         return self.cleaned_data.get('storages')
+
+    def clean_time(self):
+        """Возвращаем объект(словарь) из часов, минут и секкунд установленных пользователем.
+        """
+        if not self.cleaned_data.get('time', 0):
+            return {'hours': 0, 'minutes': 0, 'seconds': 0}
+        
+        if self.cleaned_data.get('time') == '':
+            return {'hours': 0, 'minutes': 0, 'seconds': 0}
+
+        time_digit = self.cleaned_data.get('time')
+        
+        try:
+            time_digit = int(time_digit)
+        except:
+            return {'hours': 0, 'minutes': 0, 'seconds': 0}
+        for times in TIME:
+            if times[0] == time_digit:
+                time = times[2].split(':')
+                time = {'hours': time[0], 'minutes': time[1], 'seconds': 0}
+                break
+
+        return time
