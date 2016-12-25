f3a17972a72501fd77a9a61fdef1758ce161c90e
Maxim <sfcl@mail.ru>
2016-12-25 14:50:32 +0500

    Добавил поиск по имени для вью Stock

--- index/templatetags/pagination.py
+++ index/templatetags/pagination.py
@@ -10,11 +10,18 @@ def found_pagination(items_objects, size_perpage, request=None):
     Пагинация страниц в стиле foundation 4.
     Фукция на вход принимает объект Page класса Paginator.
     """
+    search = ''
     if request and request.GET.get('search_number', 0):
         search_number = request.GET.get('search_number')
         search = '&search_number=' + str(search_number)
     else:
         search = ''
+
+    if request and request.GET.get('search_name', 0):
+        search_name = request.GET.get('search_name')
+        tmp = '&search_name=' + str(search_name)
+        search += tmp
+
     return {
         'items': items_objects, 'spp': str(size_perpage), 'search': search ,
     }
