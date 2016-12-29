79bc45c16d169f21617488bb0288f6b61f1bcdef
Maxim Khozyainov <sfcl@mail.ru>
2016-12-29 08:42:37 +0500

    Переключил в прод режим

--- conf/settings.py
+++ conf/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(BASE_DIR, 'conf'))
 
 # автоматический переключатель продакшена и девелопмента
-from settings_dev import *
+from settings_prod import *
 
 ALLOWED_HOSTS = ['*']
 
